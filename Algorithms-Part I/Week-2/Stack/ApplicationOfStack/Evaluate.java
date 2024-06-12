a556a68ebed468bd66552b4bd46d8bdbb07cb0c3
Prashant <prashantbhattarainepal@gmail.com>
2024-06-13 00:36:23 +0545

    Tokenized implementeation of above problem

--- Algorithms-Part I/Week-2/Stack/ApplicationOfStack/Evaluate.java
+++ Algorithms-Part I/Week-2/Stack/ApplicationOfStack/Evaluate.java
@@ -1,6 +1,4 @@
 //This is the java code to calculate the expression in Postfix manner
-
-
 import java.util.Stack;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -12,17 +10,20 @@ public class Evaluate {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a expression:");
+        String expression = scanner.nextLine();
         scanner.close();
 
-        while (scanner.hasNext()) {
-            String s = scanner.next();
+        //Regular expressions to split the input into tockens
+        String[] tokens = expression.split("(?<=[-+*/()])|(?=[-+*/()])");
+
+        for (String s : tokens) {
+            s = s.trim(); // remove any extra whitespace
+            if (s.isEmpty()) continue;
             if (s.equals("(")) {
-                //Do nothing for the left parenthesis
-            }
-            else if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
+                // Do nothing for left parenthesis
+            } else if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
                 ops.push(s);
-            }
-            else if (s.equals(")")) {
+            } else if (s.equals(")")) {
                 String op = ops.pop();
                 double v = vals.pop();
                 if (op.equals("+")) v = vals.pop() + v;
@@ -30,7 +31,7 @@ public class Evaluate {
                 else if (op.equals("*")) v = vals.pop() * v;
                 else if (op.equals("/")) v = vals.pop() / v;
                 vals.push(v);
-            } else{
+            } else {
                 vals.push(Double.parseDouble(s));
             }
         }
